d8a9efde5da83c7feef92f1b58b0582e4d686c0f
Safi Ullah <safiullah445@outlook.com>
2025-03-10 06:07:52 +0500

    Decryption
    4th layer added

--- caesar_DES_encryption/caesar_des_decryption.py
+++ caesar_DES_encryption/caesar_des_decryption.py
@@ -5,12 +5,14 @@ class CaesarDESCipher:
         self.shift = shift % 26
         self.additional_shift = sum(ord(c) for c in self.key) % 26  # Second round shift
         self.third_shift = (self.shift + self.additional_shift) % 26  # Third round shift
+        self.fourth_shift = (self.third_shift + self.additional_shift) % 26  # Fourth round shift
 
     def decrypt(self, ciphertext: str) -> str:
-        first_round = self._apply_caesar_cipher(ciphertext, -self.third_shift)
-        second_round = self._apply_caesar_cipher(first_round, -self.additional_shift)
-        third_round = self._apply_caesar_cipher(second_round, -self.shift)
-        return third_round
+        first_round = self._apply_caesar_cipher(ciphertext, -self.fourth_shift)
+        second_round = self._apply_caesar_cipher(first_round, -self.third_shift)
+        third_round = self._apply_caesar_cipher(second_round, -self.additional_shift)
+        fourth_round = self._apply_caesar_cipher(third_round, -self.shift)
+        return fourth_round
 
     def _apply_caesar_cipher(self, text: str, shift: int) -> str:
         return ''.join(self._shift_character(char, shift) for char in text)
